ea709550659dd5c4d81daaa302c9fb3f34f1186b
NunezTania <tnunez@bluewin.ch>
2022-01-19 11:32:19 +0100

    communication entre Auditor et Musician ok

--- docker/image-musician/src/Musician.js
+++ docker/image-musician/src/Musician.js
@@ -1,3 +1,7 @@
+const uuid = require("uuid");
+const protocol = require("./musician-protocol");
+const dgram = require('dgram');
+
 const soundDict = {};
 soundDict['piano'] = 'ti-ta-ti';
 soundDict['trumpet'] = 'pouet';
@@ -5,29 +9,8 @@ soundDict['flute'] = 'trulu';
 soundDict['violin'] = 'gzi-gzi';
 soundDict['drum'] = 'boum-boum';
 
-const protocol = require('./musician-protocol');
-
-const uuid = require('uuid');
 // We use a standard Node.js module to work with UDP
-const dgram = require('dgram');
-
 const s = dgram.createSocket('udp4');
-// Create a measure object and serialize it to JSON
-var sound = { timestamp: Date.now(),
-    instrument: process.argv[2],
-    sound: getSound(process.argv[2])
-};
-
-var payload = JSON.stringify(sound);
-
-console.log(payload);
-
-// Send the payload via UDP (multicast)
-message = new Buffer(payload);
-s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
-    function(err, bytes) {
-        console.log("Sending payload: " + payload + " via port " + s.address().port);
-    });
 
 function getSound(instrument) {
     if (instrument in soundDict) {
@@ -37,3 +20,34 @@ function getSound(instrument) {
         return 'no_sound';
     }
 }
+
+function Musician(instrument) {
+
+    this.instrument = instrument;
+    this.uuid = uuid.v4();
+    Musician.prototype.sendSound = function() {
+        var sound = { timestamp: Date.now(),
+            uuid: this.uuid,
+            instrument: this.instrument,
+            sound: getSound(this.instrument)
+        };
+
+        var payload = JSON.stringify(sound);
+
+        console.log(payload);
+
+// Send the payload via UDP (multicast)
+        message = new Buffer(payload);
+        s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
+            function(err, bytes) {
+                console.log("Sending payload: " + payload + " via port " + s.address().port);
+            });
+    }
+
+    setInterval(this.sendSound.bind(this), 1000);
+
+}
+
+var instrument = process.argv[2];
+
+var musician = new Musician(instrument);
