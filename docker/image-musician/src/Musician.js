755ce7e31ee79f5dbe21b3d274a6c77f211e9c97
NunezTania <tnunez@bluewin.ch>
2022-01-19 11:01:39 +0100

    Fichier js pour Musicien UDP

--- docker/image-musician/src/Musician.js
+++ docker/image-musician/src/Musician.js
@@ -13,13 +13,27 @@ const dgram = require('dgram');
 // Let's create a datagram socket. We will use it to send our UDP datagrams
 const s = dgram.createSocket('udp4');
 // Create a measure object and serialize it to JSON
-var measure = { timestamp: Date.now(),
-    location: this.location,
-    temperature: this.temperature };
-var payload = JSON.stringify(measure);
+var sound = { timestamp: Date.now(),
+    instrument: process.argv[2],
+    sound: getSound(process.argv[2])
+};
+
+var payload = JSON.stringify(sound);
+
+console.log(payload);
+
 // Send the payload via UDP (multicast)
 message = new Buffer(payload);
 s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
     function(err, bytes) {
         console.log("Sending payload: " + payload + " via port " + s.address().port);
     });
+
+function getSound(instrument) {
+    if (instrument in soundDict) {
+        return soundDict[instrument];
+    } else {
+        console.log("Sorry we don't have this instrument.");
+        return 'no_sound';
+    }
+}
