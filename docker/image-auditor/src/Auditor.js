3e07f29345838f6269b7535fe6090c674021663a
NunezTania <tnunez@bluewin.ch>
2022-01-26 11:02:48 +0100

    auditor debut parsing

--- docker/image-auditor/src/Auditor.js
+++ docker/image-auditor/src/Auditor.js
@@ -1,3 +1,4 @@
+var musicianDict = {};
 
 const protocol = require('../../image-musician/src/musician-protocol');
 
@@ -14,4 +15,16 @@ s.bind(protocol.PROTOCOL_PORT, function() {
 s.on('message', function(msg, source) {
     console.log("Data has arrived: " + msg + ". Source IP: " + source.address +
         ". Source port: " + source.port);
+    var data = String(msg).split(',');
+    console.log(data);
+
+    var uuid = data[1].split(':')[1].substr(1, data[1].split(':')[1].length - 2);
+    console.log(uuid);
+
+    var instrument = data[2].split(':')[2].substr(1, data[1].split(':')[2].length - 2);
+
+
+    console.log('Dict after message:\r\n');
+    console.log(musicianDict);
+
 });
