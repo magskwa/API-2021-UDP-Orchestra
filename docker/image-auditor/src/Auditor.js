4f9a0812fc463818bc3b09e3f0144b0770372992
magskwa <magali.egger@heig-vd.ch>
2022-01-26 11:58:47 +0100

    TCP SERVER

--- docker/image-auditor/src/Auditor.js
+++ docker/image-auditor/src/Auditor.js
@@ -31,6 +31,45 @@ s.on('message', function(msg, source) {
     };
 
     console.log('Dict after message:\r\n');
-    console.log(musicianDict);
+    //console.log(musicianDict);
 
+});
+
+
+
+const Net = require('net');
+const port = 2205;
+
+const server = new Net.Server();
+
+server.listen(port, function() {
+    console.log('Server listening for connection requests');
+});
+
+
+
+server.on('connection', function(socket) {
+    console.log('A new connection has been established.');
+
+    // écrire la liste des musicien
+    socket.write("Hello you\n");
+    for( value of Object.values(musicianDict)){
+        socket.write(JSON.stringify(value));
+    }
+
+    server.close(function () {
+        console.log('server closed.');
+        server.unref();
+    });
+    /*
+
+    socket.on('end', function() {
+        console.log('Closing connection with the client');
+    });
+
+     */
+
+    socket.on('error', function(err) {
+        console.log(`Error: ${err}`);
+    });
 });
